0dba8efc3baf9677a48ebc45882a4abe4426528f
Christopher Vouga <crvouga@gmail.com>
2021-01-05 14:32:47 -0700

    contact form refactored styles

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,57 +1,85 @@
 import {
   Box,
   Button,
-  Container,
+  makeStyles,
   Paper,
   TextField,
   Typography,
 } from "@material-ui/core";
 import React from "react";
 
+const useStyles = makeStyles((theme) => ({
+  form: {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: theme.breakpoints.values.sm,
+    margin: "auto",
+    padding: theme.spacing(2),
+  },
+  textField: {
+    color: theme.palette.text.primary,
+    marginBottom: theme.spacing(2),
+  },
+  submitButton: {
+    color: theme.palette.text.primary,
+    fontWeight: "bold",
+  },
+}));
+
 export const Contact = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
-      <Typography align="center" variant="h4" gutterBottom>
-        Contact
+      <Typography align="center" variant="h4">
+        Contact Me
       </Typography>
+
+      <Typography
+        align="center"
+        color="textSecondary"
+        variant="h6"
+        gutterBottom
+      >
+        Feel free to shoot me a message!
+      </Typography>
+
       <form>
-        <Container maxWidth="sm" disableGutters>
-          <Paper>
-            <Box p={1} display="flex" flexDirection="column">
-              <Box p={1}>
-                <TextField variant="outlined" fullWidth label="Name" />
-              </Box>
-              <Box p={1}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  label="Email"
-                  type="email"
-                />
-              </Box>
-              <Box p={1}>
-                <TextField
-                  variant="outlined"
-                  fullWidth
-                  multiline
-                  rows={6}
-                  label={"Your Message"}
-                />
-              </Box>
-              <Box display="flex" flexDirection="row" p={1}>
-                <Box flex={1} />
-                <Button
-                  size="large"
-                  style={{ color: "#fff" }}
-                  variant="contained"
-                  color="primary"
-                >
-                  Submit
-                </Button>
-              </Box>
-            </Box>
-          </Paper>
-        </Container>
+        <Paper className={classes.form}>
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            fullWidth
+            label="Name"
+          />
+
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            fullWidth
+            label="Email"
+            type="email"
+          />
+
+          <TextField
+            className={classes.textField}
+            variant="outlined"
+            fullWidth
+            multiline
+            rows={6}
+            label={"Your Message"}
+          />
+
+          <Box display="flex" flexDirection="row-reverse" p={1}>
+            <Button
+              className={classes.submitButton}
+              size="large"
+              variant="contained"
+              color="primary"
+            >
+              Submit
+            </Button>
+          </Box>
+        </Paper>
       </form>
     </React.Fragment>
   );
