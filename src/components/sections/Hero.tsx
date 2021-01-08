7bdec2e7c08ed755565b10a1d4f7c0973d1e7e83
Christopher Vouga <crvouga@gmail.com>
2021-01-08 12:00:48 -0700

    added greeting in hero section

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,21 +1,33 @@
-import { Box, Typography, useTheme } from "@material-ui/core";
+import { Box, Typography, useTheme, makeStyles } from "@material-ui/core";
 import React from "react";
 import content from "../../../content/content.json";
 import { SocialMediaIconButton } from "../SocialMedia";
 
+const useStyles = makeStyles(() => ({
+  bold: {
+    fontWeight: "bold",
+  },
+}));
+
 export const Hero = () => {
+  const classes = useStyles();
   const theme = useTheme();
 
   return (
     <React.Fragment>
-      <Box paddingY={2} fontWeight="bold">
-        <Typography variant="h2">Chris Vouga.</Typography>
-        <Typography variant="h2" color="textSecondary">
+      <Box paddingY={2}>
+        <Typography color="primary" className={classes.bold} variant="h6">
+          Hello, my name is
+        </Typography>
+        <Typography variant="h2" className={classes.bold}>
+          Chris Vouga.
+        </Typography>
+        <Typography variant="h2" color="textSecondary" className={classes.bold}>
           I build things for the web.
         </Typography>
       </Box>
       <Box paddingY={2} maxWidth={theme.breakpoints.values.sm}>
-        <Typography>
+        <Typography className={classes.bold} variant="h6">
           I a software engineer based the Phoenix Valley. My specialization is
           in web development.
         </Typography>
