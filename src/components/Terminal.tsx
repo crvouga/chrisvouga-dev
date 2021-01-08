8dbed4fb28f98c9e2729f2999363574ec89e051d
Christopher Vouga <crvouga@gmail.com>
2021-01-08 15:27:38 -0700

    new hero picture

--- src/components/Terminal.tsx
+++ src/components/Terminal.tsx
@@ -1,14 +1,13 @@
-import { useTheme } from "@material-ui/core";
+// import { useTheme } from "@material-ui/core";
 import React from "react";
 import ReactTerminal from "terminal-in-react";
-
 import NodeEvalPlugin from "terminal-in-react-node-eval-plugin";
 import pseudoFileSystemPlugin from "terminal-in-react-pseudo-file-system-plugin";
 
 const FileSystemPlugin = pseudoFileSystemPlugin();
 
 export const Terminal = () => {
-  const theme = useTheme();
+  // const theme = useTheme();
 
   return (
     <ReactTerminal
@@ -24,12 +23,13 @@ export const Terminal = () => {
       }}
       watchConsoleLogging={false}
       allowTabs={false}
-      color={theme.palette.text.primary}
-      outputColor={theme.palette.text.primary}
-      barColor={theme.palette.background.paper}
-      backgroundColor={theme.palette.background.paper}
-      prompt={theme.palette.text.primary}
+      // color={theme.palette.text.primary}
+      // outputColor={theme.palette.text.primary}
+      // barColor={theme.palette.background.paper}
+      // backgroundColor={theme.palette.background.paper}
+      // prompt={theme.palette.text.primary}
       // promptSymbol={"λ"}
+
       plugins={[
         FileSystemPlugin,
         {
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,32 +1,32 @@
 import {
   Box,
+  Button,
   Container,
   Grid,
   makeStyles,
   Typography,
+  Theme,
 } from "@material-ui/core";
-import clsx from "clsx";
 import React from "react";
+
+const themeToFill = (theme: Theme) =>
+  theme.palette.background.default
+    //why? "#" is a reserved character in a url
+    .replace("#", "%23");
+
+const createBackgroundUrl = (theme: Theme) =>
+  `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1123 258'><path d='M1124,2c0,0 0,256 0,256l-1125,0l0,-48c0,0 16,5 55,5c116,0 197,-92 325,-92c121,0 114,46 254,46c140,0 214,-167 572,-166Z' style='fill: ${themeToFill(
+    theme
+  )}' /></svg>`;
+
 const useStyles = makeStyles((theme) => ({
   typography: {
     fontWeight: "bold",
     color: "#fff",
   },
-  logo: {
-    width: "240px",
-  },
-  tagline: {
-    maxWidth: theme.breakpoints.values.sm,
-  },
   root: {
-    paddingTop: theme.spacing(2),
-
-    backgroundSize: "auto",
     background: `
-      url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1123 258'><path d='M1124,2c0,0 0,256 0,256l-1125,0l0,-48c0,0 16,5 55,5c116,0 197,-92 325,-92c121,0 114,46 254,46c140,0 214,-167 572,-166Z' style='fill: ${theme.palette.background.default.replace(
-        "#",
-        "%23"
-      )}' /></svg>")
+      url("${createBackgroundUrl(theme)}")
       no-repeat bottom,
       linear-gradient(
         to bottom,
@@ -36,8 +36,15 @@ const useStyles = makeStyles((theme) => ({
       )`,
   },
 
+  callToAction: {
+    fontWeight: "bold",
+    color: "white",
+    borderColor: "white",
+  },
+
   gutter: {
-    marginBottom: theme.spacing(2),
+    width: "100%",
+    height: "75px",
   },
 }));
 
@@ -47,9 +54,9 @@ export const Hero = () => {
   const classes = useStyles();
 
   return (
-    <Box className={classes.root}>
-      <Container>
-        <Grid container direction="row">
+    <section className={classes.root}>
+      <Container maxWidth="lg">
+        <Grid container direction="row" alignItems="center">
           <Grid item sm={6}>
             <Typography
               color="primary"
@@ -64,18 +71,28 @@ export const Hero = () => {
             <Typography
               variant="h2"
               color="textSecondary"
-              className={clsx(classes.tagline, classes.typography)}
+              className={classes.typography}
+              gutterBottom
             >
               I build things for the web.
             </Typography>
+            <Button
+              size="large"
+              className={classes.callToAction}
+              variant="outlined"
+            >
+              Check out work
+            </Button>
           </Grid>
 
-          <Grid item sm={4}>
-            <img width="100%" src="/hero-logo.svg" />
+          <Grid item sm={6}>
+            <Container maxWidth="xs">
+              <img width="100%" height="100%" src="/hero.svg" />
+            </Container>
           </Grid>
         </Grid>
-        <Box className={classes.gutter} />
       </Container>
-    </Box>
+      <Box className={classes.gutter} />
+    </section>
   );
 };
