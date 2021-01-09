fedcce94ee37117c344c582d22e2ddb49dde46a2
Christopher Vouga <crvouga@gmail.com>
2021-01-09 15:07:31 -0700

    moved nav bar to components folder

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -4,7 +4,7 @@ import { About } from "./about";
 import { Contact } from "./contact";
 import { Footer } from "./footer";
 import { Hero } from "./hero";
-import { NavigationBar } from "./navigation";
+import { NavigationBar } from "../../components/navigation";
 import { Projects } from "./projects";
 
 export const LandingPage = () => {
