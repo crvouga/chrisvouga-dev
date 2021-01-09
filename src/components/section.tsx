72b455914695ae480315804abad215ee7a4a9c15
Christopher Vouga <crvouga@gmail.com>
2021-01-09 13:25:00 -0700

    added types around page sections

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,5 +1,29 @@
 import { Box, BoxProps, Container } from "@material-ui/core";
 
+export enum Sections {
+  About = "About",
+  Projects = "Projects",
+  Contact = "Contact",
+}
+
+export const SECTION_TO_URL: { [section in Sections]: string } = {
+  [Sections.About]: "/#about",
+  [Sections.Contact]: "/#contact",
+  [Sections.Projects]: "/#projects",
+};
+
+export const SECTION_TO_NAME: { [section in Sections]: string } = {
+  [Sections.About]: "About",
+  [Sections.Contact]: "Contact",
+  [Sections.Projects]: "Work",
+};
+
+export const SECTION_ORDER = [
+  Sections.About,
+  Sections.Contact,
+  Sections.Projects,
+];
+
 export const Section = ({
   children,
   ...props
