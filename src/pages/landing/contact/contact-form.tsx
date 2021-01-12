b39f8a5d0225bd3af38e3a523f8697fdabdda71b
Christopher Vouga <crvouga@gmail.com>
2021-01-12 16:40:19 -0700

    contact form working

--- src/pages/landing/contact/contact-form.tsx
+++ src/pages/landing/contact/contact-form.tsx
@@ -1,90 +1,181 @@
 import {
   Box,
   Button,
+  ButtonProps,
   Card,
+  CardContent,
+  CircularProgress,
+  Container,
+  Dialog,
   Grid,
-  makeStyles,
   TextField,
+  Typography,
+  DialogActions,
 } from "@material-ui/core";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import SendIcon from "@material-ui/icons/Send";
-import React from "react";
-import { EmailAddress } from "../../../utility";
+import { dissoc } from "ramda";
+import React, { useRef, useState } from "react";
+import { submitContactForm } from "../../../services/contact-form";
+import { castEmailAddress, isValidEmailAddress } from "../../../utility";
 
-const useStyles = makeStyles((theme) => ({
-  form: {
-    display: "flex",
-    flexDirection: "column",
-    maxWidth: theme.breakpoints.values.sm,
-    margin: "auto",
-    padding: theme.spacing(2),
-  },
-  textField: {
-    color: theme.palette.text.primary,
-    marginBottom: theme.spacing(2),
-  },
-  submitButton: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-}));
+const SubmitButton = (props: ButtonProps) => {
+  return (
+    <Button
+      type="submit"
+      size="large"
+      style={{ color: "#fff", fontWeight: "bold" }}
+      variant="contained"
+      color="primary"
+      endIcon={<SendIcon />}
+      {...props}
+    >
+      Send
+    </Button>
+  );
+};
 
-export interface IContactForm {
-  submit(emailAddress: EmailAddress, message: string): Promise<void>;
-}
+const formEventToFormData = (formEvent: React.FormEvent<HTMLFormElement>) =>
+  Object.fromEntries(
+    Array.from(
+      new FormData(formEvent.currentTarget).entries()
+    ).map(([key, value]) => [key.toString(), value.toString()])
+  );
+
+type FormErrors = {
+  [name: string]: { helperText: string };
+};
 
 export const ContactForm = () => {
-  const classes = useStyles();
+  const formRef = useRef<HTMLFormElement | null>(null);
 
-  const handleSubmit = () => {
-    console.log("submit");
+  const [errors, setErrors] = useState<FormErrors>({});
+  const [status, setStatus] = useState<"success" | "loading" | "error" | null>(
+    null
+  );
+
+  const handleSubmit = async (formEvent: React.FormEvent<HTMLFormElement>) => {
+    setStatus("loading");
+    try {
+      formEvent.preventDefault();
+
+      const formData = formEventToFormData(formEvent);
+
+      const newErrors: FormErrors = {};
+
+      if (!isValidEmailAddress(formData.emailAddress)) {
+        newErrors.emailAddress = {
+          helperText: "Invalid email address",
+        };
+        setErrors(newErrors);
+        throw new Error("");
+      }
+
+      const emailAddress = castEmailAddress(formData.emailAddress);
+      const message = formData.message.toString();
+
+      await submitContactForm(emailAddress, message);
+
+      if (formRef.current) {
+        formRef.current.reset();
+      }
+      setStatus("success");
+    } catch (error) {
+      setStatus("error");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <Card className={classes.form}>
-        <Grid container spacing={1}>
-          <Grid item xs={12} sm={6}>
-            <TextField
-              className={classes.textField}
-              variant="outlined"
-              fullWidth
-              label="Name"
-            />
-          </Grid>
-
-          <Grid item xs={12} sm={6}>
-            <TextField
-              className={classes.textField}
-              variant="outlined"
-              fullWidth
-              label="Email"
-              type="email"
-            />
-          </Grid>
-        </Grid>
-
-        <TextField
-          className={classes.textField}
-          variant="outlined"
-          fullWidth
-          multiline
-          rows={6}
-          label={"Your Message"}
-        />
-
-        <Box display="flex" flexDirection="row-reverse" p={1}>
-          <Button
-            type="submit"
-            className={classes.submitButton}
-            size="large"
-            variant="contained"
-            color="primary"
-            endIcon={<SendIcon />}
-          >
-            Send
-          </Button>
+    <React.Fragment>
+      <Dialog open={status === "loading"}>
+        <Box display="flex" alignItems="center" p={2}>
+          <Box marginRight={2}>
+            <CircularProgress />
+          </Box>
+          <Typography variant="h6">Sending...</Typography>
         </Box>
-      </Card>
-    </form>
+      </Dialog>
+      <Dialog
+        open={status === "success"}
+        onClick={() => {
+          setStatus(null);
+        }}
+        onClose={() => {
+          setStatus(null);
+        }}
+      >
+        <Box p={2}>
+          <Box
+            width="100%"
+            display="flex"
+            justifyContent="center"
+            color="success.main"
+          >
+            <CheckCircleIcon
+              color="inherit"
+              style={{ width: "120px", height: "120px" }}
+            />
+          </Box>
+          <Typography align="center" variant="h5">
+            Message Sent!
+          </Typography>
+          <Typography align="center" variant="h6" color="textSecondary">
+            I'll do my best to get back to you.
+          </Typography>
+        </Box>
+      </Dialog>
+      <form onSubmit={handleSubmit} ref={formRef}>
+        <Container maxWidth="sm" disableGutters>
+          <Card>
+            <CardContent>
+              <Grid container spacing={2}>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    fullWidth
+                    label="Email Address"
+                    type="text"
+                    color="primary"
+                    name="emailAddress"
+                    error={"emailAddress" in errors}
+                    helperText={
+                      "emailAddress" in errors
+                        ? errors.emailAddress.helperText
+                        : ""
+                    }
+                    onChange={
+                      "emailAddress" in errors
+                        ? () => {
+                            setErrors(dissoc("emailAddress", errors));
+                          }
+                        : undefined
+                    }
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    color="primary"
+                    variant="outlined"
+                    fullWidth
+                    multiline
+                    rows={6}
+                    label="Message"
+                    type="text"
+                    name="message"
+                  />
+                </Grid>
+
+                <Grid container item xs direction="row-reverse">
+                  <Grid item>
+                    <SubmitButton />
+                  </Grid>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Container>
+      </form>
+    </React.Fragment>
   );
 };
--- src/utility/email-address.ts
+++ src/utility/email-address.ts
@@ -1,9 +1,11 @@
 import * as EmailValidator from "email-validator";
 
+export const isValidEmailAddress = EmailValidator.validate;
+
 export type EmailAddress = string & { readonly __type__: "EmailAddress" };
 
 export const castEmailAddress = (emailAddress: string) => {
-  if (EmailValidator.validate(emailAddress)) {
+  if (isValidEmailAddress(emailAddress)) {
     return emailAddress as EmailAddress;
   }
   throw new Error("invalid email address");
