fd6897315cc90b44f7b2772156c1cea3d562b2d2
Christopher Vouga <crvouga@gmail.com>
2021-01-22 16:37:54 -0700

    added font preload

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,14 +33,25 @@ export default class extends Document {
   }
 
   render() {
+    //why?: https://stackoverflow.com/questions/45983301/google-pagespeed-eliminate-render-blocking-resources-above-the-fold-caused-fro
+    const handleLoad = (
+      event: React.SyntheticEvent<HTMLLinkElement, Event>
+    ) => {
+      const link = event.currentTarget;
+      link.onload = null;
+      link.rel = "stylesheet";
+    };
+
     return (
       <Html lang="en">
         <Head>
           <SEO />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+
           <link
-            rel="stylesheet"
+            rel="preload"
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
+            as="style"
+            onLoad={handleLoad}
           />
         </Head>
         <body>
