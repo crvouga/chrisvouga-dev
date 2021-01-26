ce6574ab670f26b046108587259a35b672c3a3f3
Christopher Vouga <crvouga@gmail.com>
2021-01-25 21:06:06 -0700

    cleaned up SEO component

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -2,23 +2,14 @@ import React from "react";
 import { encodeUrl } from "../utility";
 import { DOMAIN } from "../personal-information";
 
-const TITLE = "Chris Vouga | Software Developer";
-
-const DESCRIPTION = `
-  Personal website for Chris Vouga. Chris Vouga is a software developer.
-`;
-
-const IMAGE = `${DOMAIN}${encodeUrl(DOMAIN)}.png`;
-
-const KEYWORDS = [
-  "chris",
-  "christopher",
-  "vouga",
-  "chrisvouga",
-  "christophervouga",
-];
-
-const AUTHOR = "Chris Vouga";
+const seo = {
+  title: "Chris Vouga | Software Developer",
+  description:
+    "Personal website for Chris Vouga. Chris Vouga is a software developer.",
+  image: `${DOMAIN}${encodeUrl(DOMAIN)}.png`,
+  keywords: ["chris", "christopher", "vouga", "chrisvouga", "christophervouga"],
+  author: "Chris Vouga",
+};
 
 export const SEO = () => {
   return (
@@ -27,29 +18,29 @@ export const SEO = () => {
 
       <link rel="icon" type="image/png" href="/personal-logo.png" />
 
-      <title>{TITLE}</title>
+      <title>{seo.title}</title>
 
-      <meta name="image" content={IMAGE} />
+      <meta name="image" content={seo.image} />
 
-      <meta name="description" content={DESCRIPTION} />
+      <meta name="description" content={seo.description} />
 
-      <meta name="keywords" content={KEYWORDS.join(", ")} />
+      <meta name="keywords" content={seo.keywords.join(", ")} />
 
-      <meta name="author" content={AUTHOR} />
+      <meta name="author" content={seo.author} />
 
-      <meta name="og:title" property="og:title" content={TITLE} />
+      <meta name="og:title" property="og:title" content={seo.title} />
 
       <meta
         name="og:description"
         property="og:description"
-        content={DESCRIPTION}
+        content={seo.description}
       />
 
       <meta property="og:url" content={DOMAIN} />
 
       <meta property="og:type" content="website" />
 
-      <meta property="og:image" content={IMAGE} />
+      <meta property="og:image" content={seo.image} />
     </React.Fragment>
   );
 };
