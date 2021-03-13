0f6af48cb8483c424b8f4d591f891410f7aca958
Christopher Vouga <crvouga@gmail.com>
2021-03-12 21:15:56 -0700

    refactored about me data

--- content/about-me.ts
+++ content/about-me.ts
@@ -4,23 +4,23 @@ Statistics from Arizona State University. Being a math major,
 I've always enjoyed learning the theory behind software development like
 type systems, architecture, and functional programming (which is the
 origin of my logo).
-`;
+`.trim();
 
 const skills = `
 The philosophy I live by is to learn and adopt technology on a
 need-to-nerd basis. In other words, focus on tech that gets the job
 done rather than what is currently fashionable.
-`;
+`.trim();
 
 const overview = `
 Hello, my name is Chris Vouga. I'm a software developer based in the
 Phoenix Valley. I find it a lot of fun to create things that live on
 the web. So it follows that most of my skills and expertise reside in
 web development. A random fact about me is that I play the drums.
-`;
+`.trim();
 
 export default {
-  overview: overview.trim(),
-  education: education.trim(),
-  skills: skills.trim(),
+  overview,
+  education,
+  skills,
 };
