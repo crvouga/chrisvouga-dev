b062c01e8e1db6e29f176a427e8afd61069cd29b
Christopher Vouga <crvouga@gmail.com>
2021-09-14 14:38:42 -0700

    tweak

---
+++ src/scripts/generate-assets/generate-assets.ts
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+import { generateAllProjectScreenshots } from "./generate-project-screenshots";
+import { generateRobotsTxt } from "./generate-robots-txt";
+
+dotenv.config();
+
+export const generateAssets = async () => {
+  await generateRobotsTxt();
+  await generateAllProjectScreenshots();
+};
--- src/scripts/generate-assets/index.ts
+++ src/scripts/generate-assets/index.ts
@@ -1,12 +1,3 @@
-import dotenv from "dotenv";
-import { generateAllProjectScreenshots } from "./generate-project-screenshots";
-import { generateRobotsTxt } from "./generate-robots-txt";
-
-dotenv.config();
-
-export const generateAssets = async () => {
-  await generateRobotsTxt();
-  await generateAllProjectScreenshots();
-};
+import { generateAssets } from "./generate-assets";
 
 generateAssets();
