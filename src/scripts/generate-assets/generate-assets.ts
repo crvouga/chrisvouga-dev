9f9313623e177e21e4f8a4d82be2e979db2b2bb6
Christopher Vouga <crvouga@gmail.com>
2021-09-16 20:48:53 -0700

    cleamed up theme logic

--- src/scripts/generate-assets/generate-assets.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import { generateRobotsTxt } from "./generate-robots-txt";
-
-dotenv.config();
-
-export const generateAssets = async () => {
-  await generateRobotsTxt();
-};
--- src/theme/theme-provider.tsx
+++ src/theme/theme-provider.tsx
@@ -1,31 +1,39 @@
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { MuiThemeProvider } from "@material-ui/core/styles";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useMemo } from "react";
 import useDarkMode from "use-dark-mode";
 import { THEME_MAP } from "./theme";
 import { useThemeStateContext } from "./theme-state";
+
+const useTheme = () => {
+  const { themeType, themeColor } = useThemeStateContext();
+
+  const darkMode = useDarkMode();
+
+  return useMemo(() => {
+    const { dark: darkTheme, light: lightTheme } = THEME_MAP[themeColor];
+
+    const systemTheme = darkMode.value ? darkTheme : lightTheme;
+
+    const theme =
+      themeType === "dark"
+        ? darkTheme
+        : themeType === "light"
+        ? lightTheme
+        : themeType === "system"
+        ? systemTheme
+        : systemTheme;
+
+    return theme;
+  }, [themeType, themeColor, darkMode.value]);
+};
 
 interface IThemeProviderProps extends PropsWithChildren<{}> {}
 
 export const ThemeProvider = (props: IThemeProviderProps) => {
   const { children } = props;
 
-  const { themeType, themeColor } = useThemeStateContext();
-
-  const { dark: darkTheme, light: lightTheme } = THEME_MAP[themeColor];
-
-  const darkMode = useDarkMode();
-
-  const systemTheme = darkMode.value ? darkTheme : lightTheme;
-
-  const theme =
-    themeType === "dark"
-      ? darkTheme
-      : themeType === "light"
-      ? lightTheme
-      : themeType === "system"
-      ? systemTheme
-      : systemTheme;
+  const theme = useTheme();
 
   return (
     <MuiThemeProvider theme={theme}>
