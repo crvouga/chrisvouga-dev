60af318d70d29d62d0498a46e531890d6d2e1234
Christopher Vouga <crvouga@gmail.com>
2022-01-02 01:40:21 -0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head";
 import { AppProps } from "next/app";
 import "../styles/index.css";
+import data from "../../data.json";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Chris Vouga - Devloper</title>
+        <title>{data.metaTitle}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content={data.metaDescription} />
       </Head>
       <Component {...pageProps} />
     </>
