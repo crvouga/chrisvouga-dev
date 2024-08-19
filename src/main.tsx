330c5fcd60c9b67ec2eda6ac0313376bc7ff2c57
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-08-19 12:32:56 -0700

    chore: Update package.json and remove unused dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,5 @@
 import '@fontsource/inter';
-import { CssBaseline, CssVarsProvider, getInitColorSchemeScript } from "@mui/joy";
 import { ViteReactSSG } from "vite-react-ssg/single-page";
-import theme from "../src/theme";
-import { App } from "./app";
-
-
-
-function Root() {
-    return (
-        <CssVarsProvider
-            defaultMode="dark"
-            theme={theme}
-            colorSchemeSelector="#demo_dark-mode-by-default"
-            modeStorageKey="demo_dark-mode-by-default"
-            disableNestedContext
-            defaultColorScheme="dark"
-        >
-            {getInitColorSchemeScript({ defaultMode: 'dark' })}
-            <CssBaseline />
-            <div id="demo_dark-mode-by-default">
-                <App />
-            </div>
-        </CssVarsProvider>
-    );
-}
+import { Root } from './root';
 
 export const createRoot = ViteReactSSG(<Root />);
---
+++ src/root.tsx
@@ -0,0 +1,23 @@
+import '@fontsource/inter';
+import { CssBaseline, CssVarsProvider, getInitColorSchemeScript } from "@mui/joy";
+import theme from "../src/theme";
+import { App } from "./app";
+
+export function Root() {
+    return (
+        <CssVarsProvider
+            defaultMode="dark"
+            theme={theme}
+            colorSchemeSelector="#demo_dark-mode-by-default"
+            modeStorageKey="demo_dark-mode-by-default"
+            disableNestedContext
+            defaultColorScheme="dark"
+        >
+            {getInitColorSchemeScript({ defaultMode: 'dark' })}
+            <CssBaseline />
+            <div id="demo_dark-mode-by-default">
+                <App />
+            </div>
+        </CssVarsProvider>
+    );
+}
