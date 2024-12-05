e6f35467bc959237745bb07abac558f71f38ba55
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2024-12-05 15:34:03 -0700

    chore: Update TypeScript configuration and remove unused files; modify Vite config and adjust root component for dark mode

--- src/root.tsx
+++ src/root.tsx
@@ -8,15 +8,14 @@ import { App } from "./app";
 export function Root() {
   return (
     <CssVarsProvider
-      defaultMode="dark"
-      colorSchemeSelector="#demo_dark-mode-by-default"
-      modeStorageKey="demo_dark-mode-by-default"
+      colorSchemeSelector="#dark-mode-by-default"
+      modeStorageKey="dark-mode-by-default"
       disableNestedContext
       defaultColorScheme="dark"
     >
       {getInitColorSchemeScript({ defaultMode: "dark" })}
       <CssBaseline />
-      <div id="demo_dark-mode-by-default">
+      <div id="dark-mode-by-default">
         <App />
       </div>
     </CssVarsProvider>
