ad4dae01897acc0a75bce6b9d891699f7d284c4d
Chris Vouga <crvouga@gmail.com>
2024-12-06 18:54:19 -0700

    feat: Remove index.html and vite.config.ts; update tsconfig.json include paths

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-// @ts-ignore
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  // @ts-ignore
-  plugins: [react()],
-});
